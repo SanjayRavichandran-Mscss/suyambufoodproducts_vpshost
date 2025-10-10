ccd012db4e7866f2090032d02ebb224281a98a17
Sanjay R <sanpro2004.jay@gmail.com>
2025-10-10 17:10:20 +0530

    remove new code for customer controller and customerroutes

--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -1,51 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const customerController = require('../controllers/customerController');
-
-// router.post('/register', customerController.register);
-// router.post('/login', customerController.login);
-// router.get('/profile', customerController.getProfile);
-
-// // New cart routes
-// router.post('/cart', customerController.addToCart);
-// router.get('/cart', customerController.getCart);
-// router.put('/cart', customerController.updateCartQuantity);
-// router.delete('/cart', customerController.deleteFromCart);
-
-// // Wishlist routes
-// router.get('/wishlist', customerController.getWishlist);
-// router.post('/wishlist', customerController.toggleWishlist);
-
-// // Address routes
-// router.get('/addresses', customerController.getAddresses);
-// router.post('/addresses', customerController.addAddress);
-// router.put('/addresses', customerController.updateAddress);
-// router.delete('/addresses', customerController.deleteAddress);
-
-// // Customer details route
-// router.get('/customer-details', customerController.getCustomerDetails);
-
-// // Order routes
-// router.post('/orders', customerController.placeOrder);
-// router.get('/orders', customerController.getOrders);
-
-// // Invoice routes (now returns HTML for frontend PDF generation)
-// router.get('/invoice/:orderId/data', customerController.getInvoiceData);
-
-// module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
 const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
@@ -77,15 +29,16 @@ router.get('/customer-details', customerController.getCustomerDetails);
 router.post('/orders', customerController.placeOrder);
 router.get('/orders', customerController.getOrders);
 
-// --- RAZORPAY PAYMENT ROUTES ---
-// This route creates the Razorpay order on the server
-router.post('/payment/create-order', customerController.createPaymentOrder);
-
-// This route verifies the payment signature after the user pays
-router.post('/payment/verify', customerController.verifyPayment);
-// ---------------------------------
-
 // Invoice routes (now returns HTML for frontend PDF generation)
 router.get('/invoice/:orderId/data', customerController.getInvoiceData);
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
