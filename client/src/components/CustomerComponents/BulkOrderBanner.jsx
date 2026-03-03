cf4677946ff2ac7d019527d752b0fce41fb222bd
Sanjay R <sanpro2004.jay@gmail.com>
2026-03-03 09:49:38 +0530

    updated bulkorderbanner for spacing

--- client/src/components/CustomerComponents/BulkOrderBanner.jsx
+++ client/src/components/CustomerComponents/BulkOrderBanner.jsx
@@ -7,17 +7,22 @@ import BulkOrderMobile from "../../Assets/BulkOrderImage/bulkorder_mobile.png";
 
 const BulkOrderBanner = () => {
   return (
-    <section className="w-full overflow-hidden">
-      <Link to="/contact" className="block w-full transition-opacity hover:opacity-95">
-        <picture>
-          <source media="(max-width: 767px)" srcSet={BulkOrderMobile} />
-          <img
-            src={BulkOrderDesktop}
-            alt="Bulk Orders - Click to Contact Us"
-            className="w-full h-auto object-cover cursor-pointer"
-          />
-        </picture>
-      </Link>
+    <section className="w-full px-4 sm:px-6 lg:px-8 xl:px-12 py-6 md:py-8">
+      <div className="mx-auto max-w-7xl">
+        <Link
+          to="/contact"
+          className="block w-full transition-opacity hover:opacity-95"
+        >
+          <picture>
+            <source media="(max-width: 767px)" srcSet={BulkOrderMobile} />
+            <img
+              src={BulkOrderDesktop}
+              alt="Bulk Orders - Click to Contact Us"
+              className="w-full h-auto object-cover rounded-lg shadow-md"
+            />
+          </picture>
+        </Link>
+      </div>
     </section>
   );
 };
